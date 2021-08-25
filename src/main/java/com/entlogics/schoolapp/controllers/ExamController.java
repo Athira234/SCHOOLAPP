ded6a15df8d8b1e09f4f060ea6e777ce0a4e1701
Athira234 <athirakoliyott1996@gmail.com>
2021-08-25 19:39:09 +0530

    Added method for Implementing GET /exams?classid=4&subjectid=1

--- src/main/java/com/entlogics/schoolapp/controllers/ExamController.java
+++ src/main/java/com/entlogics/schoolapp/controllers/ExamController.java
@@ -1,14 +1,21 @@
 package com.entlogics.schoolapp.controllers;
 
+import java.util.List;
+import java.util.ListIterator;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.entlogics.schoolapp.models.Exam;
+import com.entlogics.schoolapp.models.School;
+import com.entlogics.schoolapp.models.Student;
 import com.entlogics.schoolapp.services.IExamService;
+import com.entlogics.schoolapp.services.ISchoolService;
 
 @Controller
 @ComponentScan
@@ -20,10 +27,34 @@ public class ExamController {
 	public void setiExamService(IExamService iExamService) {
 		this.iExamService = iExamService;
 	}
+	
+	ISchoolService iSchoolService;
+
+	@Autowired
+	public void setiSchoolService(ISchoolService iSchoolService) {
+		this.iSchoolService = iSchoolService;
+	}
+
 	@RequestMapping("/exams")
-	public String exam() {
+	public String getExams(@RequestParam(value = "classId") int classId,@RequestParam(value = "subjectId") int subjectId, Model model) {
        
-		System.out.println("inside exam() method");
+		System.out.println("Inside getExams() method in Exam Controller");
+		
+		
+		List<Exam> exams = iExamService.getExams(classId,subjectId);
+
+		ListIterator litr = exams.listIterator();
+
+		while (litr.hasNext()) {
+
+			Exam exam = (Exam) litr.next();
+
+			System.out.println("Exam Object is : " + exam);
+		}
+
+		model.addAttribute("examsList", exams);
+
+	
 	
 		return "exam";
 	}
